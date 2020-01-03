bf4cc10890c89c86c561b2c1b17385a92136ae4c
wyg1997 <wyg19970408@gmail.com>
2020-01-03 13:46:14 +0800

    :tada: Modify loss function and loc-angle-size ok

--- solver/loss_functions.py
+++ solver/loss_functions.py
@@ -14,26 +14,31 @@ def l2_loss(output, target, mask=None):
                f"target: {target[0][mask[0]].view(8, -1).permute(1, 0)[:5, :]}",
                level='debug')
         cprint(output[mask] - target[mask], level='debug')
-        loss = (output[mask] - target[mask]) ** 2 / 2 / batch_size
+        loss = (output[mask] - target[mask]) ** 2 / 2
     else:
-        loss = (output - target) ** 2 / 2 / batch_size
-    return loss.sum()
+        loss = (output - target) ** 2 / 2
+    return loss.sum() / batch_size
 
 
 def angle_loss_func(output, target, mask):
     batch_size = output.shape[0]
 
-    output = output.permute(0, 2, 3, 1)
-    target = target.permute(0, 2, 3, 1)
-    mask = mask.permute(0, 2, 3, 1)
+    count = mask.sum().item()
+    if count == 0:
+        return 0
+
+    # debug
+    torch.set_printoptions(threshold=9999998, linewidth=200)
+    p_output = output.permute(0, 2, 3, 1)
+    p_target = target.permute(0, 2, 3, 1)
+    p_mask = mask.permute(0, 2, 3, 1)
+
+    # # debug
+    # cprint(p_output[p_mask].reshape(-1, 2)[:10], level='debug')
+    # cprint(p_target[p_mask].reshape(-1, 2)[:10], level='debug')
 
     # loss = 1 - sin(a)*sin(x) - cos(a)*cos(x)
-    loss = None
-    for i in range(batch_size):
-        if loss is None:
-            loss = (1 - (output[i, mask[i]].reshape(-1, 2) * target[i, mask[i]].reshape(-1, 2)).sum(axis=1)).sum()
-        else:
-            loss += (1 - (output[i, mask[i]].reshape(-1, 2) * target[i, mask[i]].reshape(-1, 2)).sum(axis=1)).sum()
+    loss = F.smooth_l1_loss(output[mask], target[mask], reduction='sum')
     return loss / batch_size
 
 
@@ -41,8 +46,11 @@ def size_loss_func(output, target, mask):
     batch_size = output.shape[0]
 
     if output[mask].sum().item() > 0:
-        loss = F.smooth_l1_loss(output[mask] / target[mask], torch.ones(output[mask].shape).cuda())
+        loss = F.smooth_l1_loss(output[mask] / target[mask],
+                                torch.ones(output[mask].shape).cuda(),
+                                reduction='sum')
     else:
         loss = 0
+
     return loss / batch_size
 
